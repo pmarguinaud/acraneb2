a61be5a35e650b8484b80107dfdf7addb0e4ace8
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2021-06-24 17:15:56 +0100

    More scripts + move local variables of DELTA_C and DELTA_T at the top level of ACRANEB_TRANST

--- src/inline.pl
+++ src/inline.pl
@@ -12,7 +12,38 @@ sub replaceArg
 {
   my ($e, $a) = @_;
 
-  die $a->toString if ($a->nodeName ne 'named-E');
+  if ($a->nodeName eq 'named-E')
+    {
+      return &replaceArgNamedE ($e, $a);
+    }
+  elsif ($a->nodeName eq 'op-E')
+    {
+      return &replaceArgOpE ($e, $a);
+    }
+  else
+    {
+      die $a->toString;
+    }
+
+}
+
+sub replaceArgOpE
+{
+  my ($e, $a) = @_;
+
+  $a = &n ('<parens-E>(' . $a->textContent . ')</parens-E>');
+
+  my @re = &f ('./f:R-LT/' . &Fxtran::xpath_by_type ('R'), $e);
+
+  die $a->toString if (@re);
+
+  $e->replaceNode ($a);
+
+}
+
+sub replaceArgNamedE
+{
+  my ($e, $a) = @_;
 
   my ($ne) = &f ('./f:N/f:n/text ()', $e);
   my ($na) = &f ('./f:N/f:n/text ()', $a);
@@ -41,11 +72,11 @@ sub replaceArg
   
       $ra = $ra->cloneNode (1);
 
-      die unless ($re->nodeName eq 'parens-R');
-      die unless ($ra->nodeName eq 'parens-R');
+      die unless ($re->nodeName =~ m/(?:parens|array)-R$/o);
+      die unless ($ra->nodeName =~ m/(?:parens|array)-R$/o);
 
       my @el = &f ('./f:element-LT/f:element/' . &Fxtran::xpath_by_type ('E'), $re);
-      my @ss = &f ('./f:array-R/f:section-subscript-LT/f:section-subscript/node ()', $ra);
+      my @ss = &f ('./f:section-subscript-LT/f:section-subscript/node ()', $ra);
 
       for (my $i = 0; $i < @el; $i++)
         {
@@ -71,12 +102,15 @@ sub replaceArg
 
 }
 
-my ($f1, $f2, $suf) = @ARGV;
+my ($f1, $suf) = @ARGV;
 
 $suf ||= '';
 
 my $d1 = &Fxtran::fxtran (location => $f1);
-my $d2 = &Fxtran::fxtran (location => $f2);
+
+my ($d2) = &f ('.//f:program-unit[./f:subroutine-stmt[./f:subroutine-N/f:N/f:n/text ()="DELTA_C"]]', $d1);
+
+$d2 = $d2->cloneNode (1);
 
 my ($s1) = &f ('.//f:subroutine-stmt', $d1);
 my ($s2) = &f ('.//f:subroutine-stmt', $d2);
@@ -109,6 +143,7 @@ for my $call (@call)
   }
 
 
+
 # Remove dummy arguments declaration
 
 for my $da (@da)
@@ -123,66 +158,10 @@ for my $da (@da)
       }
   }
 
-# Move use statements 
-
-my @use1 = &f ('.//f:use-stmt', $d1);
-
-my $stmt = $use1[-1];
-
-$stmt ||= $s1;
-
-my @use2 = &f ('.//f:use-stmt', $d2);
-
-for my $use2 (reverse (@use2))
-  {
-    my ($cr) = &f ('following::text ()[contains (., "' . "\n" . '")]', $use2);
-    $s1->parentNode ()->insertAfter ($use2, $stmt);
-    $s1->parentNode ()->insertAfter (&t ("\n"), $stmt);
-    $cr->unbindNode ();
-  }
-
-my @en_decl1 = &f ('.//f:EN-decl', $d1);
-my @en_decl2 = &f ('.//f:EN-decl', $d2);
-
-# Remove comments before declarations
-
-if (@en_decl2)
-  {
-    my @C = &f ('preceding::f:C', $en_decl2[0]);
-    for my $C (@C)
-      {
-        my ($cr) = &f ('following::text ()[contains (., "' . "\n" . '")]', $C);
-        $C->unbindNode ();
-        $cr->unbindNode ();
-      }
-  }
-
 # Remove first and last statements
 
 $s2->parentNode ()->lastChild ()->unbindNode ();
 $s2->unbindNode ();
-
-# Move declarations
-
-for my $en_decl2 (@en_decl2)
-  {
-
-    my ($v2) = &f ('./f:EN-N/f:N/f:n/text ()', $en_decl2);
-
-    my ($v1) = &f ('.//f:EN-decl[./f:EN-N/f:N/f:n[text ()="?"]]', $v2->textContent, $d1);
-
-    # This variable exists in first subroutine
-    if (($v1 || $suf) && ($v2 !~ m/^(?:JLON|JLEV|JROF)$/o))
-      {
-        my $s = $suf || "_$n2";
-        $v2->replaceNode (&t ("$v2$suf"));
-        my @e2 = &f ('.//f:named-E/f:N/f:n[text ()="?"]/text ()', $v2, $d2);
-        for my $e2 (@e2) 
-          {
-            $e2->replaceNode (&t ("$v2$suf"));
-          }
-      }
-  }
 
 # Replace dummy arguments by actual arguments
 for my $da (@da)
@@ -196,37 +175,20 @@ for my $da (@da)
   }
 
 
-my @decl_stmt1 = &f ('.//f:' . &Fxtran::xpath_by_type ('stmt') . '[.//f:EN-decl]', $d1);
-
-my $decl_stmt1 = $decl_stmt1[-1];
-
-for my $decl_stmt2 (&f ('.//f:' . &Fxtran::xpath_by_type ('stmt') . '[.//f:EN-decl]', $d2))
-  {
-    $decl_stmt1->parentNode->insertAfter ($decl_stmt2, $decl_stmt1);
-    $decl_stmt1->parentNode->insertAfter (&t ("\n"), $decl_stmt1);
-  }
-
-for (&f ('.//f:include', $d2))
-  {
-    $_->unbindNode (); # Should move includes to d1
-  }
 for (&f ('.//f:implicit-none-stmt', $d2))
   {
     $_->unbindNode (); # Should move includes to d1
   }
-for (&f ('.//f:broken-stmt[./text ()="init_stack ()"]', $d2))
-  {
-    $_->unbindNode (); 
-  }
-
 
 for my $call (@call)
   {
-    my @stmt = &f ('.//f:program-unit/node ()', $d2);
+    my @stmt = &f ('descendant-or-self::f:program-unit/node ()', $d2);
+
     for my $stmt (@stmt)
       {
         $call->parentNode->insertBefore ($stmt, $call);
       }
+
     my $c = $call->textContent ();
     $c =~ s/\n/\n! /goms;
     $c = "! $c";
@@ -240,6 +202,6 @@ for my $call (@call)
 
 
 'FileHandle'->new (">$f1.new")->print ($d1->textContent ());
-'FileHandle'->new (">$f2.new")->print ($d2->textContent ());
+'FileHandle'->new (">delta_c.F90.new")->print ($d2->textContent ());
 
 
