66dfe54cf8752419e68c4b276a262d34db29f168
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2021-06-25 14:29:32 +0100

    Move DELTA_S local variables at top level

--- src/underscore.pl
+++ src/underscore.pl
@@ -8,13 +8,13 @@ use FindBin qw ($Bin);
 use lib $Bin;
 use Fxtran;
 
-my ($f1) = @ARGV;
+my ($f1, @n2) = @ARGV;
 
 my $d1 = &Fxtran::fxtran (location => $f1);
 
-for my $sub (qw (DELTA_C DELTA_T))
+for my $n2 (@n2)
   {
-    my ($d2) = &f ('.//f:program-unit[./f:subroutine-stmt[./f:subroutine-N/f:N/f:n/text ()="?"]]', $sub, $d1);
+    my ($d2) = &f ('.//f:program-unit[./f:subroutine-stmt[./f:subroutine-N/f:N/f:n/text ()="?"]]', $n2, $d1);
     
     # Suffix local variables with a _
     
