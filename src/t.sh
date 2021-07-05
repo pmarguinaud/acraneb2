24b405300b8c9ceffa58fa66bc59316ca520cd08
Philippe Marguinaud <pmarguinaud@hotmail.com>
2021-07-05 09:44:38 +0200

    Reshape data into a single block

--- src/t.sh
+++ src/t.sh
@@ -7,41 +7,22 @@
 #SBATCH --gres=gpu:2
 
 
-cd /gpfswork/rech/jau/ufh62jk/acraneb2/openacc-kernels/src
+cd /gpfswork/rech/jau/ufh62jk/acraneb2/openacc-kernels-reshape/src
 module load nvidia-compilers/21.5
 
 set -x
 set -e
 
-git=/gpfsstore/rech/jau/ufh62jk/install/git-2.32.0/bin/git
-
-echo "============="
-
-
 ./scripts/compile.pl --arch gpu --update  --compile 
 
 
-if [ 0 -eq 1 ]
-then
-
-cd compile.gpu
-status=$($git status --porcelain)
-if [ "x$status" != "x" ]
-then
-$git commit -a -m "Compiles"
-fi
-$git log | head -1
-cd ..
-fi
-
 cd ../data
 
-for i in $(seq 1)
-do
-../src/compile.gpu/main.x --nproma 20480 --ncount 10 --ngpblk   1 --check
-done
+#xport PGI_ACC_NOTIFY=1
 
-echo "============="
+../src/compile.gpu/main.x --nproma    32 --ncount 10 --ngpblk 640 --check
+
+exit
 
 ../src/compile.gpu/main.x --nproma  4096 --ncount 10 --ngpblk   5 --check
 ../src/compile.gpu/main.x --nproma  2048 --ncount 10 --ngpblk  10 --check
@@ -52,9 +33,3 @@ echo "============="
 ../src/compile.gpu/main.x --nproma    64 --ncount 10 --ngpblk 320 --check
 ../src/compile.gpu/main.x --nproma    32 --ncount 10 --ngpblk 640 --check
 
-echo "============="
-
-for i in $(seq 1)
-do
-../src/small_kernels/main --nproma 20480 --ncount 10 --ngpblk   1 --check
-done
