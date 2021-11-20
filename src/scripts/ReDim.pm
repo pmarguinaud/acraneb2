76c2fbe59622469016e6f026f825b26dedee6d3b
Philippe Marguinaud <pmarguinaud@hotmail.com>
2021-11-20 18:41:52 +0100

    Drop KLON dimension for arrays with fixed dimensions (eg Z (KLON, 2))

--- src/scripts/ReDim.pm
+++ src/scripts/ReDim.pm
@@ -12,25 +12,57 @@ sub reDim
 {
   my $d = shift;
   
-  my @en_decl = &F ('.//EN-decl[./array-spec/shape-spec-LT[count(shape-spec)=1][string(shape-spec)="?"]]', 'KLON', $d);
+  my @en_decl = &F ('.//EN-decl[./array-spec/shape-spec-LT[string(shape-spec)="?"]]', 'KLON', $d);
   
-  for my $en_decl (@en_decl)
+  EN_DECL : for my $en_decl (@en_decl)
     {
       my ($N) = &F ('./EN-N', $en_decl, 1);
       my ($stmt) = &Fxtran::stmt ($en_decl);
 
       next if (&F ('.//attribute-N[string(.)="INTENT"]', $stmt));
       next if (&F ('.//call-stmt[.//named-E[string(N)="?"]', $N, $d));
-
   
       my ($as) = &F ('./array-spec', $en_decl);
-      $as->unbindNode ();
-  
+
+      my @ss = &F ('./shape-spec-LT/shape-spec', $as);
+
+      for my $ss (@ss[1..$#ss])
+        {
+          my ($lb) = &F ('./lower-bound/*', $ss);
+          my ($ub) = &F ('./upper-bound/*', $ss);
+          for ($lb, $ub)
+            {
+              next unless ($_);
+              next EN_DECL unless ($_->nodeName eq 'literal-E');
+            }
+        }
+
+      if (@ss > 1)
+        {
+          $ss[0]->nextSibling->unbindNode ();
+          $ss[0]->unbindNode ();  
+        }
+      else
+        {
+          $as->unbindNode ();
+        }
+
       my @rlt = &F ('.//named-E[string(N)="?"]/R-LT', $N, $d);
   
       for my $rlt (@rlt)
         {
-          $rlt->unbindNode ();
+          my ($r) = &F ('./ANY-R[1]', $rlt);
+          die unless ($r->nodeName eq 'parens-R');
+          my @e = &F ('./element-LT/element', $r);
+          if (scalar (@e) > 1)
+            {
+              $e[0]->nextSibling->unbindNode ();
+              $e[0]->unbindNode ();
+            }
+          else
+            {
+              $rlt->unbindNode ();
+            }
         }
   
     }
