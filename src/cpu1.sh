b0177db8378f87a2024ccb7993070cb60cc07344
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2022-03-01 08:25:01 +0000

    Privatize variables of contained SUBROUTINEs of acraneb_transt.F90

--- src/cpu1.sh
+++ src/cpu1.sh
@@ -14,21 +14,21 @@ ulimit -s unlimited
 export OMP_STACKSIZE=8Gb
 
 
-for arch in cpu_s cpu_d
+for arch in cpu_d
 do
   ./scripts/compile.pl --arch $arch --update --compile
 done
 
 cd ../data
 
-for arch in cpu_s cpu_d
+for arch in cpu_d
 do
 
   cp ../src/linux_bind.txt .
   export OMP_NUM_THREADS=1
-  ../src/compile.$arch/main.x  --nproma  32 --ngpblk 1 --save --check > ../src/$arch.eo 2>&1
-  mkdir -p $arch
-  mv *.dat $arch/.
+  ../src/compile.$arch/main.x  --nproma  32 --ngpblk 1 --save --check # > ../src/$arch.eo 2>&1
+# mkdir -p $arch
+# mv *.dat $arch/.
 
 done
 
