e3244085fc7119d86c9dd4eaca316d478140a03b
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2023-01-03 18:30:32 +0000

    Single allocation (IALLOC==3)

--- src/gpu.sh
+++ src/gpu.sh
@@ -9,31 +9,47 @@ set -e
 
 cd /scratch/work/marguina/acraneb2/single-directive-redim-sp-stack-alloc/src
 
-export NV_ACC_CUDA_HEAPSIZE=64Mb
+nvidia-smi
 
 cd ../data
+
+../src/compile.gpu_d/main.x  --nproma  32 --ngpblk   1 --ncount  1 --save --check  --alloc 1
+
+for alloc in 1 2 3
+do
+
+if [ "x$alloc" = "x3" ]
+then
+  unset NV_ACC_CUDA_HEAPSIZE
+  heapsize=16000
+else
+  export NV_ACC_CUDA_HEAPSIZE=64Mb
+  heapsize=0
+fi
 
 #for arch in gpu_s gpu_d 
 for arch in gpu_d 
 do
 
-  ../src/compile.$arch/main.x  --nproma  32 --ngpblk   1 --ncount  1 --save --check  
+# ../src/compile.$arch/main.x  --nproma  32 --ngpblk   1 --ncount  1 --save --check  --alloc $alloc --heapsize $heapsize
+# ../src/compile.$arch/main.x  --nproma  32 --ngpblk   2 --ncount  1 --save --check  --alloc $alloc --heapsize $heapsize
+# ../src/compile.$arch/main.x  --nproma  32 --ngpblk  10 --ncount  1 --save --check  --alloc $alloc --heapsize $heapsize
 
-  nvidia-smi
   if [ "$arch" = "gpu_s" ]
   then
-  ../src/compile.$arch/main.x  --nproma  32 --ngpblk 3840 --ncount 10 # --save --check  
-  ../src/compile.$arch/main.x  --nproma  32 --ngpblk 2560 --ncount 10 # --save --check  
-  ../src/compile.$arch/main.x  --nproma  32 --ngpblk 1600 --ncount 10 # --save --check  
+  ../src/compile.$arch/main.x  --nproma  32 --ngpblk 3840 --ncount 10 --alloc $alloc --heapsize $heapsize # --save --check  
+  ../src/compile.$arch/main.x  --nproma  32 --ngpblk 2560 --ncount 10 --alloc $alloc --heapsize $heapsize # --save --check  
+  ../src/compile.$arch/main.x  --nproma  32 --ngpblk 1600 --ncount 10 --alloc $alloc --heapsize $heapsize # --save --check  
   fi
-# ../src/compile.$arch/main.x  --nproma  32 --ngpblk 1281 --ncount 10 # --save --check  
-  ../src/compile.$arch/main.x  --nproma  32 --ngpblk 1280 --ncount 10 # --save --check  
-  ../src/compile.$arch/main.x  --nproma  32 --ngpblk  960 --ncount 10 # --save --check  
-  ../src/compile.$arch/main.x  --nproma  32 --ngpblk  640 --ncount 10 # --save --check  
-  ../src/compile.$arch/main.x  --nproma  32 --ngpblk  480 --ncount 10 # --save --check  
-  ../src/compile.$arch/main.x  --nproma  32 --ngpblk  320 --ncount 10 # --save --check  
-  ../src/compile.$arch/main.x  --nproma  32 --ngpblk  160 --ncount 10 # --save --check  
-  ../src/compile.$arch/main.x  --nproma  32 --ngpblk   80 --ncount 10 # --save --check  
+  ../src/compile.$arch/main.x  --nproma  32 --ngpblk   80 --ncount 10 --alloc $alloc --heapsize $heapsize # --save --check  
+  ../src/compile.$arch/main.x  --nproma  32 --ngpblk  160 --ncount 10 --alloc $alloc --heapsize $heapsize # --save --check  
+  ../src/compile.$arch/main.x  --nproma  32 --ngpblk  320 --ncount 10 --alloc $alloc --heapsize $heapsize # --save --check  
+  ../src/compile.$arch/main.x  --nproma  32 --ngpblk  480 --ncount 10 --alloc $alloc --heapsize $heapsize # --save --check  
+  ../src/compile.$arch/main.x  --nproma  32 --ngpblk  640 --ncount 10 --alloc $alloc --heapsize $heapsize # --save --check  
+  ../src/compile.$arch/main.x  --nproma  32 --ngpblk  960 --ncount 10 --alloc $alloc --heapsize $heapsize # --save --check  
+  ../src/compile.$arch/main.x  --nproma  32 --ngpblk 1280 --ncount 10 --alloc $alloc --heapsize $heapsize # --save --check  
+# ../src/compile.$arch/main.x  --nproma  32 --ngpblk 1281 --ncount 10 --alloc $alloc --heapsize $heapsize # --save --check  
   
-done
+done | grep ZTC > ../src/acraneb2.$alloc.dat
 
+done
